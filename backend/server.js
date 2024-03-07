e7b01d4bfc747ddf50dc7bbde521666d07ce18a2
enbanbunbun123 <enbanbunbun-123@docomo.ne.jp>
2024-03-07 22:56:52 +0900

    feat: 3001ばんでlistenするバックエンド追加

---
+++ backend/server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const cors = require("cors");
+const app = express();
+const PORT = 3001;
+
+// CORSの設定
+app.use(cors());
+
+app.set("port", process.env.PORT || 3001);
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+// 画像を取得するエンドポイント
+app.get("/get-image", (req, res) => {
+  const directoryPath = "/Users/takahashiyuuho/Desktop/a";
+  fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+
+    const imageFiles = files.filter((file) =>
+      /\.(jpg|jpeg|png|gif)$/i.test(file)
+    );
+    if (imageFiles.length > 0) {
+      res.send({ imageName: imageFiles[0] });
+    } else {
+      res.send({ imageName: "" });
+    }
+  });
+});
+
+// 画像を移動するエンドポイント
+app.get("/move-image/:imageName", (req, res) => {
+  const { imageName } = req.params;
+  const oldPath = path.join("/Users/takahashiyuuho/Desktop/a", imageName);
+  const newPath = path.join("/Users/takahashiyuuho/Desktop/b", imageName);
+
+  fs.rename(oldPath, newPath, (err) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.send({ message: "Image moved successfully" });
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
